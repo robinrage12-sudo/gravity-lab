da45128dd190d89fdcb3384eab862a6abd42512f
robinrage12-sudo <robinrage12@gmail.com>
2026-03-09 20:06:04 +0100

    fix: remove .next from repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FV0RHQYomTzzdgeYyu7J2Kgd+bJUhCYse9Fgvv3oQpI=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_a8c03fea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_page_tsx_dda9881a._.js",
-  "static/chunks/node_modules_6e697b06._.js"
-],
-    source: "dynamic"
-});
